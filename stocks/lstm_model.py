7175f482ce6492e0be9ac2cf1dd3ef2639caa9b6
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2025-02-03 22:02:59 -0800

    lstm updates

---
+++ stocks/lstm_model.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+def create_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))  # Prediction of the next price
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+def train_lstm_model(data):
+    # Prepare the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+    # Create training data
+    training_data_len = int(np.ceil(len(scaled_data) * 0.8))
+    train_data = scaled_data[0:training_data_len, :]
+
+    x_train, y_train = [], []
+    for i in range(60, len(train_data)):
+        x_train.append(train_data[i-60:i, 0])
+        y_train.append(train_data[i, 0])
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+    # Reshape for LSTM
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    # Create and train the model
+    model = create_lstm_model((x_train.shape[1], 1))
+    model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+    return model, scaler
+
+def predict_next_price(model, scaler, last_60_days):
+    last_60_days_scaled = scaler.transform(last_60_days)
+    X_test = []
+    X_test.append(last_60_days_scaled)
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+    predicted_price = model.predict(X_test)
+    predicted_price = scaler.inverse_transform(predicted_price)
+    return predicted_price[0][0] 
--- stocks/views.py
+++ stocks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .utils import fetch_stock_data, create_candlestick_chart
+from .lstm_model import train_lstm_model, predict_next_price
 
 def stock_view(request):
     ticker = request.GET.get('ticker', 'AAPL')
@@ -7,10 +8,17 @@ def stock_view(request):
     interval = request.GET.get('interval', '1d')
     data = fetch_stock_data(ticker, period, interval)
     chart = create_candlestick_chart(data)
+
+    # Train LSTM model and make predictions
+    model, scaler = train_lstm_model(data)
+    last_60_days = data['Close'][-60:].values.reshape(-1, 1)
+    predicted_price = predict_next_price(model, scaler, last_60_days)
+
     context = {
         'ticker': ticker,
         'data': data.to_html(classes='table table-striped'),
         'chart': chart,
+        'predicted_price': predicted_price,
     }
     return render(request, 'stocks/stock_view.html', context)
 
